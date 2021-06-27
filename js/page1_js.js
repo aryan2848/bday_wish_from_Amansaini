9070b9780bdf2e43e9413e467670a0e37c8fbd18
aryan2848 <57109732+aryan2848@users.noreply.github.com>
2021-06-27 09:50:17 +0530

    js added

---
+++ js/page1_js.js
@@ -0,0 +1,198 @@
+
+alert("Ohh, Hello User");
+var key1 = prompt("Please Enter your secret key..");
+
+
+if (key1 == "aman10"){
+    var name1 = "Aman Saini";
+    var date1 = "12.10.2001";
+    var img_addr1 = "img/aman1.jpg";
+    var img_addr2 = "img/aman2.jpg";
+    var img_addr3 = "img/aman3.jpg";
+}
+
+else if (key1 == "aryan04"){
+    var name1 = "Aaryan Saini";
+    var date1 = "27.06.2004";
+    var img_addr1 = "img/anu1.jpg";
+    var img_addr2 = "img/anu2.jpg";
+    var img_addr3 = "img/anu3.jpg";
+}
+
+
+else if (key1 == "nehayog") {
+    var name1 = "Neha Saini";
+    var date1 = "3 July";
+    var img_addr1 = "img/neha1.jpg";
+    var img_addr2 = "img/neha2.jpg";
+    var img_addr3 = "img/neha3.jpg";
+}
+
+else if (key1 == "rohit22") {
+    var name1 = "Rohit Saini";
+    var date1 = "16 July";
+    var img_addr1 = "img/rohit1.jpg";
+    var img_addr2 = "img/rohit2.jpg";
+    var img_addr3 = "img/rohit3.jpg";
+}
+
+else if (key1 == "simi") {
+    var name1 = "Simranjeet Saini";
+    var date1 = "24 July";
+    var img_addr1 = "img/simi1.jpg";
+    var img_addr2 = "img/simi2.jpg";
+    var img_addr3 = "img/simi3.jpg";
+}
+
+else if (key1 == "honey99") {
+    var name1 = "Harneet Saini";
+    var date1 = "9 August";
+    var img_addr1 = "img/hny1.jpg";
+    var img_addr2 = "img/hny2.jpg";
+    var img_addr3 = "img/hny3.jpg";
+}
+
+else if (key1 == "dhruvil29") {
+    var name1 = "Dhruvil Saini";
+    var date1 = "29 August";
+    var img_addr1 = "img/dhuvil1.jpg";
+    var img_addr2 = "img/dhuvil2.jpg";
+    var img_addr3 = "img/dhuvil3.jpg";
+}
+
+else if (key1 == "deppidriver") {
+    var name1 = "Gurdeep Saini";
+    var date1 = "4 October";
+    var img_addr1 = "img/depi1.jpg";
+    var img_addr2 = "img/depi2.jpg";
+    var img_addr3 = "img/depi3.jpg";
+}
+
+else if (key1 == "radheji") {
+    var name1 = "Priyanka Saini";
+    var date1 = "9 October";
+    var img_addr1 = "img/prinka1.jpg";
+    var img_addr2 = "img/prinka2.jpg";
+    var img_addr3 = "img/prinka3.jpg";
+}
+
+else if (key1 == "judomaster") {
+    var name1 = "Harman Saini";
+    var date1 = "17 November";
+    var img_addr1 = "img/hamu1.jpg";
+    var img_addr2 = "img/hamu2.jpg";
+    var img_addr3 = "img/hamu3.jpg";
+}
+
+else if (key1 == "reena") {
+    var name1 = "Urvashi Saini";
+    var date1 = "11 December";
+    var img_addr1 = "img/uv1.jpg";
+    var img_addr2 = "img/uv2.jpg";
+    var img_addr3 = "img/uv3.jpg";
+}
+
+else if (key1 == "commando") {
+    var name1 = "Manisha Saini";
+    var date1 = "26 December";
+    var img_addr1 = "img/mns1.jpg";
+    var img_addr2 = "img/mns2.jpg";
+    var img_addr3 = "img/mns3.jpg";
+}
+
+else if (key1 == "chotapolice") {
+    var name1 = "Heavil Saini";
+    var date1 = "23 January";
+    var img_addr1 = "img/hvl1.jpg";
+    var img_addr2 = "img/hvl2.jpg";
+    var img_addr3 = "img/hvl3.jpg";
+}
+
+else if (key1 == "santoshbua") {
+    var name1 = "Santosh Buaji";
+    var date1 = "2 March";
+    var img_addr1 = "img/sn1.jpg";
+    var img_addr2 = "img/sn2.jpg";
+    var img_addr3 = "img/snn3.jpg";
+}
+
+
+else if (key1 == "shivamgamer") {
+    var name1 = "Shivam Saini";
+    var date1 = "6 March";
+    var img_addr1 = "img/svm1.jpg";
+    var img_addr2 = "img/svm2.jpg";
+    var img_addr3 = "img/svm3.jpg";
+}
+
+else if (key1 == "jonybro") {
+    var name1 = "Jony Saini";
+    var date1 = "30 March";
+    var img_addr1 = "img/jony1.jpg";
+    var img_addr2 = "img/jony2.jpg";
+    var img_addr3 = "img/jony3.jpg";
+}
+
+else if (key1 == "mataji") {
+    var name1 = "Mamtesh Rani";
+    var date1 = "30 March";
+    var img_addr1 = "img/mmy1.jpg";
+    var img_addr2 = "img/mmy2.jpg";
+    var img_addr3 = "img/mmy3.jpg";
+}
+
+else if (key1 == "ushabuaji") {
+    var name1 = "Usha Buaji";
+    var date1 = "20 April";
+    var img_addr1 = "img/us1.jpg";
+    var img_addr2 = "img/us2.jpg";
+    var img_addr3 = "img/us3.jpg";
+}
+
+else if (key1 == "papaji") {
+    var name1 = "Bikram Singh";
+    var date1 = "1 May 1973";
+    var img_addr1 = "img/ddy1.jpg";
+    var img_addr2 = "img/ddy2.jpg";
+    var img_addr3 = "img/ddy3.jpg";
+}
+
+
+else if (key1 == "nishu99") {
+    var name1 = "Anisha Saini";
+    var date1 = "11 May";
+    var img_addr1 = "img/nishu1.jpg";
+    var img_addr2 = "img/nishu2.jpg";
+    var img_addr3 = "img/nishu3.jpg";
+}
+
+else if (key1 == "shinu06") {
+    var name1 = "Shivani Saini";
+    var date1 = "6 June";
+    var img_addr1 = "img/shinu1.jpg";
+    var img_addr2 = "img/shinu2.jpg";
+    var img_addr3 = "img/shinu3.jpg";
+}
+
+
+else{
+    document.location.href = "error_page.html";
+}
+
+
+var s_v = (Math.floor((Math.random()*5) + 1));
+var s_val = "'song" + s_v + ".mp3'";
+
+
+document.write("<font color = 'yellow' size = '10'  face = 'Comic sans MS'><b><h1>" + name1 + "</h1></b></font>");
+document.write("<font color = 'red' size  = '5'  face = 'Comic sans MS'><i><h1>Bday :" + date1 + " </h1></i></font>");
+document.write("<img src=" + img_addr1 + " style='padding: 3px; border: solid green;' height='350' width='330' align='center' align='bottom' alt='no image'></img>");
+document.write("<img src=" + img_addr2 + " style='padding: 3px; border: solid red;' height='280' width='240' border='7' alt='no image'></img>");
+document.write("<img src=" + img_addr3 + " style='padding: 3px; border: solid orange;' height='350' width='330' align='center' border='5' align='bottom' alt='no image'></img>");
+
+document.write("<br><br>");
+
+document.write("<i><font color = 'purple' size = '4' >Click Below to Play your Bday Song<br><br> </font></i>");
+
+document.write("<audio controls autoplay> <source src=" + s_val + " type='audio/mpeg'> Your browser does not support audio.</audio>");
+
